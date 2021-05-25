aad028283b11106fc4ca0f668d3a96498c83b9f0
Pallavi Balwani <pallavi.balwani@impetus.com>
2021-05-25 17:45:16 +0530

    first repo

--- target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.impetus.maven_example1"}]
--- target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.impetus.maven_example1","l":"App"}]
